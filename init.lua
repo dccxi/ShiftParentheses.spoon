bcf6c07e422c239e4294734ae263432c782a0b17
Tong Wu <tongwu@live.com>
2019-03-02 13:26:26 -0800

    Modify: generate parentheses & locate between them on Shift tapping

--- init.lua
+++ init.lua
@@ -1,69 +1,71 @@
 --- === ControlEscape ===
 ---
---- Make the `control` key more useful: If the `control` key is tapped, treat it
---- as the `escape` key. If the `control` key is held down and used in
---- combination with another key, then provide the normal `control` key
---- behavior.
+--- Make the `shift` key more useful: If the `shift` key is tapped, send a
+--- pair of parentheses and locate the cursor between them. If the `shift`
+--- key is held down and used in combination with another key, then provide
+--- the normal `shift` key behavior.
 
 local obj={}
 obj.__index = obj
 
 -- Metadata
-obj.name = 'ControlEscape'
+obj.name = 'ShiftParentheses'
 obj.version = '0.1'
 obj.author = 'Jason Rudolph <jason@jasonrudolph.com>'
-obj.homepage = 'https://github.com/jasonrudolph/ControlEscape.spoon'
+obj.homepage = 'https://github.com/dccxi/ShiftParentheses.spoon'
 obj.license = 'MIT - https://opensource.org/licenses/MIT'
 
 function obj:init()
-  self.sendEscape = false
+  self.sendPerenteses = false
   self.lastModifiers = {}
 
-  -- If `control` is held for this long, don't send `escape`
+  -- If `shift` is held for this long, don't send parentheses
   local CANCEL_DELAY_SECONDS = 0.150
-  self.controlKeyTimer = hs.timer.delayed.new(CANCEL_DELAY_SECONDS, function()
-    self.sendEscape = false
+  self.shiftKeyTimer = hs.timer.delayed.new(CANCEL_DELAY_SECONDS, function()
+    self.sendPerenteses = false
   end)
 
   -- Create an eventtap to run each time the modifier keys change (i.e., each
   -- time a key like control, shift, option, or command is pressed or released)
-  self.controlTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged},
+  self.shiftTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged},
     function(event)
       local newModifiers = event:getFlags()
 
       -- If this change to the modifier keys does not invole a *change* to the
-      -- up/down state of the `control` key (i.e., it was up before and it's
+      -- up/down state of the `shift` key (i.e., it was up before and it's
       -- still up, or it was down before and it's still down), then don't take
       -- any action.
-      if self.lastModifiers['ctrl'] == newModifiers['ctrl'] then
+      if self.lastModifiers['shift'] == newModifiers['shift'] then
         return false
       end
 
-      -- If the `control` key has changed to the down state, then start the
-      -- timer. If the `control` key changes to the up state before the timer
-      -- expires, then send `escape`.
-      if not self.lastModifiers['ctrl'] then
+      -- If the `shift` key has changed to the down state, then start the
+      -- timer. If the `shift` key changes to the up state before the timer
+      -- expires, then send parenthesis.
+      if not self.lastModifiers['shift'] then
         self.lastModifiers = newModifiers
-        self.sendEscape = true
-        self.controlKeyTimer:start()
+        self.sendPerenteses = true
+        self.shiftKeyTimer:start()
       else
-        if self.sendEscape then
-          hs.eventtap.keyStroke({}, 'escape', 0)
+        if self.sendPerenteses then
+          hs.eventtap.keyStroke({'shift'}, '9', 0)
+          hs.eventtap.keyStroke({'shift'}, '0', 0)
+          hs.eventtap.keyStroke({}, 'left', 0)
         end
         self.lastModifiers = newModifiers
-        self.controlKeyTimer:stop()
+        self.shiftKeyTimer:stop()
       end
       return false
     end
   )
 
   -- Create an eventtap to run each time a normal key (i.e., a non-modifier key)
-  -- enters the down state. We only want to send `escape` if `control` is
-  -- pressed and released in isolation. If `control` is pressed in combination
-  -- with any other key, we don't want to send `escape`.
+  -- enters the down state. We only want to send parenthesis if `shift` is
+  -- pressed and released in isolation. If `shift` is pressed in combination
+  -- with any other key, we don't want to send parenthesis.
   self.keyDownEventTap = hs.eventtap.new({hs.eventtap.event.types.keyDown},
     function(event)
-      self.sendEscape = false
+      self.sendPerenteses = false
       return false
     end
   )
@@ -71,23 +73,23 @@ end
 
 --- ControlEscape:start()
 --- Method
---- Start sending `escape` when `control` is pressed and released in isolation
+--- Start sending parenthesis when `shift` is pressed and released in isolation
 function obj:start()
-  self.controlTap:start()
+  self.shiftTap:start()
   self.keyDownEventTap:start()
 end
 
 --- ControlEscape:stop()
 --- Method
---- Stop sending `escape` when `control` is pressed and released in isolation
+--- Stop sending parenthesis when `shift` is pressed and released in isolation
 function obj:stop()
   -- Stop monitoring keystrokes
-  self.controlTap:stop()
+  self.shiftTap:stop()
   self.keyDownEventTap:stop()
 
   -- Reset state
-  self.controlKeyTimer:stop()
-  self.sendEscape = false
+  self.shiftKeyTimer:stop()
+  self.sendPerenteses = false
   self.lastModifiers = {}
 end
 
